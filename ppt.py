0ca06a5b9b886e896d25fce2bbe4abd7b0fcf9e0
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-13 16:11:08 -0300

    add condicional IF y funcion

--- ppt.py
+++ ppt.py
@@ -1,7 +1,23 @@
-" Piedra, papel o tijera."
-
 import ramdom
+
 
 def jugar():
     usuario = input("Escoge una opción: 'pi' para piedra, 'pa' para papel, 'ti' para tijera.").lower()
     computadora = ramdom.choice(['pi','pa','ti'])
+
+    if usuario == computadora:
+        return '¡Empate!' 
+
+    if ganó_el_jugador(usuario,computadora):
+        return '¡Ganaste!'
+
+    return '¡Perdiste!'
+
+
+def ganó_el_jugador(jugador, oponente):
+    if ((jugador == 'pi' and oponente == 'ti')
+        or (jugador == 'ti' and oponente == 'pa')
+        or (jugador == 'pa' and oponente == 'pi')):
+        return True
+    else:
+        return False
