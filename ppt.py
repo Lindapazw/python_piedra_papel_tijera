5f9fe1ee9746b56f42e7f622db576b0de631e4de
Lindapazw <lindacsndelapaz@gmail.com>
2023-01-13 15:56:13 -0300

    add funcion

--- ppt.py
+++ ppt.py
@@ -1,3 +1,7 @@
 " Piedra, papel o tijera."
 
+import ramdom
+
 def jugar():
+    usuario = input("Escoge una opción: 'pi' para piedra, 'pa' para papel, 'ti' para tijera.").lower()
+    computadora = ramdom.choice(['pi','pa','ti'])
